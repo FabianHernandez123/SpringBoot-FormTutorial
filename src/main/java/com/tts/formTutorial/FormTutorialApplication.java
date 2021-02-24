0748750900ced47575489d6f78e9276f822edf01
Fabian Hernandez <h.fabian25f@gmail.com>
2021-02-23 20:09:40 -0600

    Completed Form Tutorial

---
+++ src/main/java/com/tts/formTutorial/FormTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.tts.formTutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tts/formTutorial/FormTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.formTutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
